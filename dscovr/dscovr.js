92f3832e51efe3ee08a3795eaf7f607dd593f26f
Nathan <nathan.frost@gmail.com>
2017-03-12 00:07:27 +0000

    add getImage function

--- dscovr/dscovr.js
+++ dscovr/dscovr.js
@@ -2,15 +2,29 @@ const fetch = require('node-fetch')
 const dscovrMocks = require('../mocks/dscovr.mock.js')
 
 const baseApi = 'https://epic.gsfc.nasa.gov/api'
+const baseArchive = 'https://epic.gsfc.nasa.gov/archive'
+
+function getJson() {
+    return fetch(`${baseApi}/natural`).then(function(res) {
+        return res.json()
+    })
+}
+
+function getImage() {
+    return getJson().then(function(json) {
+        var date = json[0].date
+        var image = json[0].image
+        const dateUrl = date.slice(0,10).replace(new RegExp('-', 'g'), '/')
+        return fetch(`${baseArchive}/natural/${dateUrl}/png/${image}.png`, function(res) {
+            return res.json()
+        })
+    })
+}
 
 module.exports = {
-    getJson: function() {
-        return fetch(`${baseApi}/natural`).then(function(res) {
-            return res.json();
-        }).then(function(json) {
-            return json
-        });
-    },
-    baseApi
+    getJson,
+    getImage,
+    baseApi,
+    baseArchive,
 }
 
--- dscovr/dscovr.spec.js
+++ dscovr/dscovr.spec.js
@@ -3,22 +3,18 @@ const nock = require('nock')
 const dscovr = require('./dscovr.js')
 const dscovrMocks = require('../mocks/dscovr.mock.js')
 
-describe('Mocha', function() {
-    describe('Finds a test', function() {
-        it('runs the test', function() {
-            assert.equal('cats', 'cats')
-        })
-    })
-})
-
 describe('dscovr', function() {
-    describe('Calls /api/fetch', function() {
+    describe('getJson', function() {
         nock(dscovr.baseApi).get('/natural').reply(200, dscovrMocks.natural)
         it('Returns JSON', function() {
             return dscovr.getJson()
                 .then(function(json) {
                     assert.deepEqual(json, dscovrMocks.natural)
                 })
+        })
+    })
+    describe('getImage', function() {
+        it('downloads latest image', function() {
         })
     })
 })
